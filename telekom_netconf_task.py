d0689e2eb5247de31b2c2af5fd6619d5d6420297
baboy0715 <babai.balint.2005@gmail.com>
2026-01-16 02:17:43 +0100

    Implement IPv4 address validation using Regex

--- telekom_netconf_task.py
+++ telekom_netconf_task.py
@@ -1,8 +1,15 @@
-from statistics import harmonic_mean
-from tkinter import EXCEPTION
 from ncclient import manager
+import re
+def validate_ip(ip):
+    pattern = r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
+    if re.match(pattern, ip):
+        return True
+    return False
 def main():
     target_ip =input("Type IP Address: ")
+    if not validate_ip(target_ip):
+        print("Error: This is not a valid IPv4 address!")
+        return
     user = input("Username: ")
     passw = input("Password: ")
     try:
@@ -21,7 +28,7 @@ def main():
             print(config.data_xml)
                
     except Exception as e:
-        print("f\nError {e}")
+        print(f"\nError {e}")
         
 
 if __name__ == "__main__":
